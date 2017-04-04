14360436a497159c6428903ab30e475ed5514f30
tolits123 <junjun_leonardo@yahoo.com>
2017-04-03 22:00:08 -0700

    Test change in Module1.vb
    added comment

--- ERS/Module1.vb
+++ ERS/Module1.vb
@@ -16,6 +16,7 @@ Module Module1
         cn.ConnectionString = "server= '" & server & "';port= '" & port & "';userid= '" & user & "';password= '" & password & "';database='" & database & "'"
         cn.Open()
 
+        'change
         'Splash screen if database is null
         Dim stm As String = "SELECT * FROM admin WHERE EmployeeID IS NOT NULL"
         Dim cmd As MySqlCommand = New MySqlCommand(stm, cn)
